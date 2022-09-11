a1e14af4dd2e0911503790c35d307da00ae159e6
ManuelP2 <manuelperezp22@hotmail.com>
2022-09-11 14:24:09 -0400

    Discover listo, fetch video listo

--- utils/client.ts
+++ utils/client.ts
@@ -1,7 +1,7 @@
 import sanityClient from '@sanity/client';
 
 export const client = sanityClient({
-  projectId: 'tp4ln3j8',
+  projectId: 'mdw45vtt',
   dataset: 'production',
   apiVersion: '2022-03-10',
   useCdn: false,
