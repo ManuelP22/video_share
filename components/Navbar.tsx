b4ebfab6dfb2fb3159a4c0885d5e3b14fb1d0c2d
ManuelP2 <manuelperezp22@hotmail.com>
2022-09-12 12:10:47 -0400

    GoogleOAuth2 deprectated cambio a react-oatuh/gool

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,26 +2,39 @@ import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { GoogleLogin, GoogleLogout} from 'react-google-login';
 import { AiOutlineLogout } from 'react-icons/ai';
+import { GoogleLogin, googleLogout } from '@react-oauth/google'
 import { BiSearch } from 'react-icons/bi';
 import { IoMdAdd } from 'react-icons/io';
 import Logo from '../utils/tiktik-logo.png'
 
-const Navbar = () => (
-    
-    <div className="w-full flex justify-between items-center border-b-2 bprder-gray-200 py-2 px-4">
-      <Link href="/">
-          <div className="w-[100px] md:w-[130px] md:[h-30px]">
-              <Image 
-                className="cursor-pointer"
-                src={Logo}
-                alt="tiktik"
-                layout="responsive"
-              />
+const Navbar = () => {
+    const user = false;
+
+    return (
+        <div className="w-full flex justify-between items-center border-b-2 bprder-gray-200 py-2 px-4">
+          <Link href="/">
+              <div className="w-[100px] md:w-[130px] md:[h-30px]">
+                  <Image 
+                    className="cursor-pointer"
+                    src={Logo}
+                    alt="tiktik"
+                    layout="responsive"
+                  />
+              </div>
+          </Link>
+
+          <div>SEARCH</div>
+
+          <div>
+            {user ? (
+              <div>Logged in</div>
+            ) : <GoogleLogin  
+                onSuccess={(response) => {console.log(response)}} 
+                onError={() => console.log('Error')}/>}
           </div>
-      </Link>
-    </div>
-)
+        </div>
+    )
+}
 
 export default Navbar
